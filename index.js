48e41c55feac0b79fa7a02be7dcce198675a259c
Ukobaandrew <101108667+Ukobaandrew@users.noreply.github.com>
2022-05-16 14:42:39 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var andy=document.getElementsByClassName("items")
+var eli=document.getElementsByClassName("menuBtn")
+var isreal=document.getElementsByClassName("menu")
+if (andy.style.width=250px){
+    eli.style.display
+}else (andy.style.width=-250px){
+    andy=
+
+}
